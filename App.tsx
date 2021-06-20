59cadc750d55d9d95d4905441b2818539fcaff49
Kaijonsson <axel.jonsson@hotmail.com>
2021-06-20 06:26:13 +0200

    separated navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-import {NavigationContainer} from "@react-navigation/native"
-import "react-native-gesture-handler"
-import { createStackNavigator } from '@react-navigation/stack';
-
 import { createStore } from 'redux';
 import {Provider} from "react-redux"
 import reducers from "./redux/store"
 
-import LoginRegScreen from "./screens/LoginRegScreen"
-import ChooseUserNameScreen from "./screens/ChooseUsernameScreen"
-import ChatScreen from "./screens/ChatScreen"
-
-import globalStyle from "./css/globalStyle"
-
-import {RootStackParamList} from "./types/types"
+import Navigation from './navigation/Navigation';
 
 import config from "./firebase"
 import firebase from "firebase";
@@ -27,22 +17,12 @@ if (!firebase.apps.length) {
 
 export default function App() {
 
-  
- 
-  const RootStack = createStackNavigator<RootStackParamList>()
-
   const store = createStore(reducers)
 
 
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <RootStack.Navigator initialRouteName="LoginRegScreen" screenOptions={{headerShown: false}}>
-        <RootStack.Screen options={globalStyle.headerOptions} name="LoginRegScreen" component={LoginRegScreen}/>
-        <RootStack.Screen options={globalStyle.headerOptions} name="ChooseUserNameScreen" component={ChooseUserNameScreen}/>
-        <RootStack.Screen options={globalStyle.headerOptions} name="ChatScreen" component={ChatScreen}/>
-      </RootStack.Navigator>
-    </NavigationContainer>
+      <Navigation/>
     </Provider>
   );
 }
---
+++ navigation/Navigation.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import globalStyle from '../css/globalStyle';
+
+import {NavigationContainer} from "@react-navigation/native"
+import "react-native-gesture-handler"
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import LoginRegScreen from "../screens/LoginRegScreen"
+import ChooseUserNameScreen from "../screens/ChooseUsernameScreen"
+import ChatScreen from "../screens/ChatScreen"
+
+import {RootStackParamList} from "../types/types"
+
+
+
+const Navigation = () => {
+
+    const RootStack = createStackNavigator<RootStackParamList>()
+
+
+    return (
+        <NavigationContainer>
+        <RootStack.Navigator initialRouteName="LoginRegScreen" screenOptions={{headerShown: false}}>
+          <RootStack.Screen options={globalStyle.headerOptions} name="LoginRegScreen" component={LoginRegScreen}/>
+          <RootStack.Screen options={globalStyle.headerOptions} name="ChooseUserNameScreen" component={ChooseUserNameScreen}/>
+          <RootStack.Screen options={globalStyle.headerOptions} name="ChatScreen" component={ChatScreen}/>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    )
+}
+
+export default Navigation
+
