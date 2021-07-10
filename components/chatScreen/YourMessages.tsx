fa77751aabf06eb40672799e30c9d8c3defe6769
Kaijonsson <axel.jonsson@hotmail.com>
2021-07-10 08:19:24 +0200

    flatlist rendering messages

--- components/chatScreen/YourMessages.tsx
+++ components/chatScreen/YourMessages.tsx
@@ -13,20 +13,14 @@ interface userObjectProp {
     },
 }
 
-interface flatlistItem {
-        message: string,
-        id: string,
-        timeStamp: string,
-}
-
 const YourMessages = ({userCredentials}: userObjectProp) => {
 
-    const [input, setInput] = useState([{}])
+    const [input, setInput] = useState<String[]>([])
 
-    console.log("cred: ", userCredentials)
     useEffect(()=> {
         firebase.database().ref("users/" + userCredentials.id + "/posts").on("child_added", (snapshot)=> {
-            setInput(input => [...input, snapshot])
+            console.log("child.val: ", snapshot.child("message").val())
+            setInput(input => [...input, snapshot.child("message").val()])
         })
     }, [])
 
@@ -40,23 +34,16 @@ const YourMessages = ({userCredentials}: userObjectProp) => {
         );
       };
 
-    const renderItem: ListRenderItem<flatlistItem> = ({item})=> {
-        console.log("item: ", item)
-        console.log("item.message: ", item.message)
-        return (
-            <View style={styles.flatlist}>
-                <Text style={styles.text}>{userCredentials.name}</Text>
-                <Text style={styles.text}>{item.message}</Text>
-            </View>
-            )
-    }
-
-    [{}]
-
     return (
         <View>
-            <Text style={styles.text}>Hej</Text>
-            <FlatList data={input} renderItem={renderItem} extraData={input} ItemSeparatorComponent={itemSeparator} keyExtractor={(item, index) => index.toString()}
+            <FlatList data={input} renderItem={({item})=> {
+                return (
+                   <View style={styles.flatlist}>
+                        <Text style={styles.text}>{userCredentials.name}</Text>
+                        <Text style={styles.text}>{item}</Text>
+                    </View> 
+                )
+            }} extraData={input} ItemSeparatorComponent={itemSeparator} keyExtractor={(item, index) => index.toString()}
 />
         </View>
     )
@@ -66,10 +53,11 @@ export default YourMessages
 
 const styles = StyleSheet.create({
     text: {
-        color: "black",
         fontSize: globalStyle.textFontSize,
     },
     flatlist: {
-        backgroundColor: "gray",
+        backgroundColor: globalStyle.mainColorGreen,
+        minWidth: 0,
+
     }
 })
